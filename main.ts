cc3227800025752d079ff891cca24e2fb7d8bdb4
Ayesha-Hafeez123 <161064150+Ayesha-Hafeez123@users.noreply.github.com>
2024-05-04 23:09:50 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+// Importing inquirer
+import inquirer from "inquirer"; 
+
+//Welcome message
+console.log("Code with Ayesha Word_Counter");
+
+//prompt the user to enter a Sentnce
+
+const answers: {
+          Sentence: string
+}= await inquirer.prompt([
+     {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+     }
+])
+
+// Triming the sentences and splitting it into words baed on "spaces"
+const words = answers.Sentence.trim().split(" ")
+console.log(words);
+
+//Analysis of user input sentence
+console.log(`Your sentence word count is ${words.length}`);
+
+
+
+
